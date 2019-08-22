0883d4dcf5dd7798823837895b8c22913db94cb7
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-08-22 10:40:57 -0500

    init commit

---
+++ src/main/java/com/TP20192/SRVTFL/SrvtflApplication.java
@@ -0,0 +1,13 @@
+package com.TP20192.SRVTFL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvtflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvtflApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TP20192/SRVTFL/SrvtflApplicationTests.java
@@ -0,0 +1,16 @@
+package com.TP20192.SRVTFL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrvtflApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
